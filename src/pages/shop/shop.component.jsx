0a7d5a2fa1ada454049e8f27f544f3b3ecdbee88
tuyen1998 <trantuyen25598@gmail.com>
2020-01-10 15:01:02 +0700

    updata shop.component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
-import CollectionOverview from '../../components/collections-overview/collections-overview.component';
 import { Route } from 'react-router-dom';
-import CollectionPage from '../collection/collection.component'
 import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
 import { connect } from 'react-redux';
-import WithSpinner from '../../components/with-spinner/with-spinner.component';
-import { createStructuredSelector } from 'reselect';
-import { selectIsCollectionFetching, selectIsCollectionsLoaded } from '../../redux/shop/shop.selector';
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
 import CollectionPageContainer from '../collection/collection.container';
 
-const CollectionsOverviewWithSpinner = WithSpinner(CollectionOverview);
-const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 
 class ShopPage extends React.Component {
