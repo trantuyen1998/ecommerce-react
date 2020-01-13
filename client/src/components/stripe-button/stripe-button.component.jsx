5fd7a77978e8d7a505ae260cda37f72c8a3382bf
tuyen1998 <trantuyen25598@gmail.com>
2020-01-13 16:17:54 +0700

    refactory code to server striper and express.js

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import axios from 'axios';
 import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
@@ -7,7 +7,23 @@ const StripeCheckoutButton = ({ price }) => {
     const publishableKey = 'pk_test_yZwlOcgQ0UeI9MNr3FdbJlWd00ncLPyKVf';
 
     const onToken = token =>{
-    console.log(token)
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+              amount: priceForStripe,
+              token: token
+            }
+          })
+            .then(response => {
+              alert('succesful payment');
+            })
+            .catch(error => {
+              console.log('Payment Error: ', error);
+              alert(
+                'There was an issue with your payment! Please make sure you use the provided credit card.'
+              );
+            });
     }
     return (
         <StripeCheckout
