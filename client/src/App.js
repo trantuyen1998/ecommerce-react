f028ed21cc1a5b723a35f07372d07ca0dd49606a
tuyen1998 <trantuyen25598@gmail.com>
2020-01-15 22:43:04 +0700

    support mobile UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import './App.css';
 import Homepage from './pages/homepages/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
@@ -10,13 +9,14 @@ import { createStructuredSelector } from 'reselect';
 import { selectCurrentUser } from './redux/user/user.selectors';
 import CheckoutPage from './pages/checkout/checkout.component';
 import { checkUserSession } from './redux/user/user.action';
-
+import { GlobalStyle } from './global.styles';
 const  App = ({checkUserSession, currentUser}) => {
   useEffect(() => {
     checkUserSession()
   },[checkUserSession])
     return (
       <div>
+        <GlobalStyle/>
         <Header />
         <Switch>
           <Route exact path='/' component={Homepage} />
